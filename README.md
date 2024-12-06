f5bdb0754823ea832d0284c1d59250daa275c2cb
eHarshavardhan N <dedlinux@gmail.com>
2024-12-06 22:50:35 +0530

    reset using head~1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # This is just a simple python file
+
+## This is branch2 and new script 
--- simple.py
+++ simple.py
@@ -1,1 +1,9 @@
 print("Hello")
+age = 12
+print(age)
+if(age<10):
+    print("less than 10")
+else:
+    print("Greater than 10")
+print("Hi")
+
